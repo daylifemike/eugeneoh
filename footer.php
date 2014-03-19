d5b268cb178592c4af261960ca396c7351c04c7b
Michael Kafka <michael.kafka@thomsonreuters.com>
2014-03-19 12:29:29 -0400

    removes datGUI

--- footer.php
+++ footer.php
@@ -12,10 +12,8 @@ $theme_url = get_template_directory_uri();
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 
-        
         <script src="<?php echo $theme_url ?>/js/plugins.js"></script>
         <script src="<?php echo $theme_url ?>/js/main.js"></script>
-        <script src="<?php echo $theme_url ?>/js/gui.js"></script>
         <script src="<?php echo $theme_url ?>/includes/shadowbox/shadowbox.js"></script>
         <script>
             (function($) {
--- header.php
+++ header.php
@@ -18,11 +18,8 @@ $theme_url = get_template_directory_uri();
         <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
         <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
-        <!--
-        <link rel="stylesheet" href="<?php echo $theme_url ?>/includes/fontAwesome/css/font-awesome.min.css" />
-        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/3.2.0/css/font-awesome.css" />
-        -->
         <link rel="stylesheet" href="<?php echo $theme_url ?>/includes/shadowbox/shadowbox.css" />
+        <link rel="stylesheet" href="<?php echo $theme_url ?>/css/normalize.css" />
         <link rel="stylesheet" href="<?php echo $theme_url ?>/style.css" />
 
         <?php echo eyo_pageMetaTags(); ?>

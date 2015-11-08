d041f3ede23af2731f2568bd3fd1ebc17f16638f
Michael Kafka <michael.kafka@thomsonreuters.com>
2015-11-07 20:09:24 -0500

    adds social icons and bumps to v1.5

--- header.php
+++ header.php
@@ -15,9 +15,10 @@ $theme_url = get_template_directory_uri();
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
-        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
-        <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css" />
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
+        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Raleway" />
         <link rel="stylesheet" href="<?php echo $theme_url ?>/includes/shadowbox/shadowbox.css" />
         <link rel="stylesheet" href="<?php echo $theme_url ?>/css/normalize.css" />
         <link rel="stylesheet" href="<?php echo $theme_url ?>/style.css" />
@@ -30,8 +31,25 @@ $theme_url = get_template_directory_uri();
     <body>
 
         <div class="container">
-        
+
             <header class="row">
                 <h1><?php bloginfo( 'title' ); ?></h1>
                 <h2><?php echo html_entity_decode( get_bloginfo( 'description' ) ); ?></h2>
+                <div class="social">
+                    <a href="https://www.facebook.com/eugene.oh.58?fref=ts" target="_blank" class="facebook" title="Facebook">
+                        <i class="fa fa-facebook"></i>
+                    </a>
+                    <a href="https://twitter.com/eugeyoung" target="_blank" class="twitter" title="Twitter">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                    <a href="http://www.imdb.me/eugeneyoung" target="_blank" class="imdb" title="IMDb">
+                        <i class="fa fa-video-camera"></i>
+                    </a>
+                    <a href="http://eugeneyoung.tumblr.com" target="_blank" class="tumblr" title="Tumblr">
+                        <i class="fa fa-tumblr"></i>
+                    </a>
+                    <a href="mailto:realeugeneyoung@gmail.com" target="_blank" class="contact" title="Contact Me">
+                        <i class="fa fa-envelope"></i>
+                    </a>
+                </div>
             </header>
